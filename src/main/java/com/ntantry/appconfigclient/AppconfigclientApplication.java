ffbd172ecaf7f5950c10917aa8dca7eab9912202
Nagaraja Tantry <nagaraja_tantry@intuit.com>
2017-10-02 12:24:07 -0700

    initial

---
+++ src/main/java/com/ntantry/appconfigclient/AppconfigclientApplication.java
@@ -0,0 +1,12 @@
+package com.ntantry.appconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppconfigclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppconfigclientApplication.class, args);
+	}
+}
